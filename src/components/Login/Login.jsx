d0cfa896b7c79824e3a145aa8eb107093b8f2b16
Miguel <mirkouda@gmail.com>
2022-12-13 17:49:42 +0100

    Register & Login added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const[formData, setFormData]=useState({
+        email:"",
+        password:"",
+    })
+    const {email, password}=formData
+    const onChange=(e)=>{
+        setFormData((prevState)=>({
+            ...prevState,
+            [e.target.name]:e.target.value,
+        }))
+    }
+    const onSubmit=(e)=>{
+        e.preventDefault()
+        console.log("formData",formData)
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input type="email" name="email" value={email} onChange={onChange} placeholder="Email"/>
+
+            <input type="password" name='password' value={password} onChange={onChange} placeholder="Password" />
+            <button type='submit'>Login</button>
+        </form>
+    )
 }
 
 export default Login
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+    const { name, email, password } = formData
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log("formData", formData)
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input type="text" name="name" value={name} onChange={onChange} placeholder="Name"/>
+            <input type="email" name="email" value={email} onChange={onChange} placeholder="Email" />
+            <input type="password" name="password" value={password} onChange={onChange} placeholder="Password" />
+            <button type="submit">Register</button>
+        </form>
+    )
 }
 
 export default Register
