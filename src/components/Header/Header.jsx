9a7ec1c1bc94de1f9975d60c07c9f2fa40e75eb4
Miguel <mirkouda@gmail.com>
2023-01-07 13:53:46 +0100

    Logo added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { logout } from '../../features/auth/authSlice'
-import { LoginOutlined, LogoutOutlined, UserOutlined, HomeOutlined, UserAddOutlined } from "@ant-design/icons"
+import { LoginOutlined, LogoutOutlined, UserOutlined, HomeOutlined, UserAddOutlined, CrownOutlined } from "@ant-design/icons"
 import "./Header.scss";
 
 
@@ -11,6 +11,11 @@ const Header = () => {
     const dispatch = useDispatch()
     const { user } = useSelector((state) => state.auth)
     const [text, setText] = useState("")
+    const [selectedImage] = useState(0);
+    const images = [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShLAUtTplbtUI1TRzj_Q3yyCsUZ3839tkpCQ&usqp=CAU",
+    ];
+
     const handleChange = (e) => {
         setText(e.target.value)
         if (e.key === "Enter") {
@@ -26,11 +31,11 @@ const Header = () => {
     }
     return (
         <nav className='navbar'>
+            <img src={images[selectedImage]} alt="" width={50} height={50} />
 
             <Link to="/"><HomeOutlined /></Link>
 
-
-            {user?.user?.role === 'admin' ? <span><Link to="/admin">Admin</Link>
+            {user?.user?.role === 'admin' ? <span><Link to="/admin"><CrownOutlined /> Admin</Link>
             </span> : ''}
 
             {user ?
