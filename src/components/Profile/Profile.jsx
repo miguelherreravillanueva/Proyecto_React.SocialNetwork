ec40043819f7f39d81efed813f54c76fc7b197b2
Miguel <mirkouda@gmail.com>
2023-01-08 12:47:54 +0100

    Profile changed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
-import { Spin, Card } from 'antd';
-import { MailOutlined, UserOutlined, EditOutlined, VideoCameraOutlined, TrophyOutlined, UsergroupDeleteOutlined, SettingOutlined, FileImageOutlined } from "@ant-design/icons"
+import { Spin, Card, Avatar } from 'antd';
+import { EditOutlined, UsergroupDeleteOutlined, SettingOutlined } from "@ant-design/icons"
 import { Link } from "react-router-dom";
 import "./Profile.scss"
 
 
 const Profile = () => {
   const { user } = useSelector((state) => state.auth)
-  const [selectedImage] = useState(0);
-  const images = [
-    "https://static.vecteezy.com/system/resources/previews/007/226/475/non_2x/user-account-circle-glyph-color-icon-user-profile-picture-userpic-silhouette-symbol-on-white-background-with-no-outline-negative-space-illustration-vector.jpg",
-  ];
+  const { Meta } = Card;
 
   if (!user) {
     return <p>Loading...</p>
@@ -23,31 +20,31 @@ const Profile = () => {
       {!user ?
         <Spin size="large" /> :
         <div className="site-card-border-less-wrapper ">
-
           <Card
-            title={user.user.name}
-            bordered={false}
             style={{
-              width: 300,
-              border: "1px solid lightGray",
-              background: "transparent",
-              color: "#47311d",
+              width: 250,
             }}
+            cover={
+              <img
+                alt="example"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkj6hvEG2-NPPdwc_KTYECZC2mqNUNGHP4JA&usqp=CAU"
+              />
+            }
+            actions={[
+              <div className='actions'>
+                <span>
+                  <Link to="*"><SettingOutlined /></Link>
+                  <Link to="*"><EditOutlined /></Link>
+                  <Link to="*"><UsergroupDeleteOutlined /></Link>
+                </span>
+              </div>
+            ]}
           >
-            <img src={images[selectedImage]} alt="" width={100} height={100} />
-            <br />
-            <p><Link to="*">Edit picture</Link></p>
-            <hr />
-            <UserOutlined /> <VideoCameraOutlined /> <FileImageOutlined /> <TrophyOutlined /> <SettingOutlined />
-            <br />
-            <br />
-            <p><MailOutlined /> {user.user.email}</p>
-            <p>
-              <UsergroupDeleteOutlined /><Link to="*">Friends</Link>
-            </p>
-            <p>
-              <EditOutlined /><Link to="*">Edit profile</Link>
-            </p>
+            <Meta
+              avatar={<Avatar src="https://static.vecteezy.com/system/resources/previews/004/297/855/non_2x/abstract-blue-circle-shape-logo-concept-global-network-icon-template-blue-round-emblem-on-white-background-isolated-vetor-illustration-vector.jpg" />}
+              title={user.user.name}
+              description={user.user.email}
+            />
           </Card>
         </div>
       }
